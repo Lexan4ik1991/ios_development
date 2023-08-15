5fcc3b566e8e7eac1e91989a799cc702fee9cbde
Alexey <112006201+Lexan4ik1991@users.noreply.github.com>
2023-08-15 03:23:10 -0700

    Add files via upload

---
+++ Hwork_storyboard/Hwork_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hwork_storyboard
+//
+//  Created by Алексей on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
